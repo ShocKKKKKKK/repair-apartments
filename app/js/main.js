50f09e6003f62808add975e248ae5e0434675747
ShocKKKKKKK <sergey.zhmursky@gmail.com>
2020-03-08 07:11:07 +0200

    added desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,34 @@
-$(function(){
+$(function () {
+  $('.btn').on('click', function () {
+    $('.modal').addClass('modal-active');
+  });
+  $('.modal__close').on('click', function () {
+    $('.modal').removeClass('modal-active');
+  });
 
+  var btn = $('#up');
 
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      btn.addClass('up-show');
+    } else {
+      btn.removeClass('up-show');
+    }
+  });
 
-  
+  btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, '300');
+  });
+
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: '.slick-prev',
+    nextArrow: '.slick-next',
+  });
 });
